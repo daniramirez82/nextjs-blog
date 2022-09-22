413eccbd2d73911a3c951bba5c6fcb9f077ef82d
dANIEL JD <dani.ramirez.madrid@gmail.com>
2022-09-22 15:48:46 +0000

    images right fit

--- components/cards/MainCard.jsx
+++ components/cards/MainCard.jsx
@@ -1,28 +1,35 @@
-import Image from 'next/image';
-import styles from './MainCard.module.css'
-import Link from 'next/link';
+import Image from "next/image";
+import styles from "./MainCard.module.css";
+import Link from "next/link";
 
 const MainCard = ({ data }) => {
+  const { description, image, url, date, title } = data;
 
-    const {description, image, url, date, title} = data;
-
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.image}>
-               { image ?  <img src={image} alt={title}/> : <img src={"/default.jpg"} alt={title}/> }
-            <Image src={image} width={1600} height={1600} layout={'responsive'}/>
-            </div>
-            <div className={styles.content}>
-                <div className={styles.date}>{date}</div>
-                <Link href={url.toString()}>
-                <h3 className={styles.title}>{title}</h3>
-                </Link>
-                <div className={styles.intro}>
-                    <p>{description}</p>
-                </div>
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.image}>
+        
+          <Image
+            src={image}
+            width={1600}
+            height={800}
+            layout={'responsive'}
+            objectFit={'cover'}
+            
+          />
+        
+      </div>
+      <div className={styles.content}>
+        <div className={styles.date}>{date}</div>
+        <Link href={url.toString()}>
+          <h3 className={styles.title}>{title}</h3>
+        </Link>
+        <div className={styles.intro}>
+          <p>{description}</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default MainCard;
